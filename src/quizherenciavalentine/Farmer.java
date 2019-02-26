c30419c939888476cfb3f993d47b4e2ec9917a43
Valentine Chimezie <Valentine Chimezie@PERSONAL>
2019-02-26 03:00:58 -0500

    Quiz ya casi acabado

--- src/quizherenciavalentine/Farmer.java
+++ src/quizherenciavalentine/Farmer.java
@@ -5,6 +5,7 @@
  */
 package quizherenciavalentine;
 
+import com.sun.org.apache.bcel.internal.generic.AALOAD;
 import java.util.HashMap;
 
 /**
@@ -12,12 +13,12 @@ import java.util.HashMap;
  * @author ancardenas
  */
 public class Farmer extends Eslabon{
-    private HashMap<String,Producto>productosquecultiva;
-
+   
+ private HashMap<String,Producto>productosquecultiva=new HashMap<>();
 
     public Farmer( String nombre, String descripcion, double latitud, double longitud) {
         super(nombre, descripcion, latitud, longitud);
-   
+    this.productosquecultiva=new HashMap<>();
     }
 
     public HashMap<String, Producto> getProductosquecultiva() {
@@ -28,27 +29,32 @@ public class Farmer extends Eslabon{
         this.productosquecultiva = productosquecultiva;
     }
 
-  private Producto CrearProductoF(String Nombre,String Fecha,Farmer f){
-      Producto p=new Producto(Nombre);
-      
+  public Producto CrearProductoF(String Nombre,String Fecha,Farmer f,String ID){
+      Producto p=new Producto(Nombre,ID);
       FechaDePaso a=new FechaDePaso(Fecha, f);
       p.getFechas().put(Fecha, a);
-      this.productosquecultiva.put(p.getNombre(),p);
+      this.productosquecultiva.put(p.getID(),p);
       return  p;
   } 
-          @Override
-    public Eslabon LugarDeEnvio(Producto producto,String fecha) {
-     return productosquecultiva.get(producto.getNombre()).getFechas().get(fecha).getLugardeproceso();
+         
+  @Override
+    public Eslabon EnviarProducto(String ID,String fechasalida,String FechaDeExpedicion,Eslabon m) {
+   
+    productosquecultiva.get(ID).getFechas().get(FechaDeExpedicion).setFecha(fechasalida);
+    productosquecultiva.get(ID).getFechas().get(FechaDeExpedicion).setLugardeproceso(m);
+     Manufacturer y = (Manufacturer) m;
+     HashMap<String,Producto>A=new HashMap<>();
+    
+   y.getProductosqueproduce().put(fechasalida,y.CrearProductoM(ID,productosquecultiva.get(ID).getNombre(),fechasalida,y,A));
+    m=y;
+    return m;
+    } @Override
+    public Eslabon LugarDondeSeEnvio(String ID,String fecha) {
+     return productosquecultiva.get(ID).getFechas().get(fecha).getLugardeproceso();
     }
 
   
-  @Override
-    public void EnviarProducto(Producto producto,String fecha,Eslabon m) {
-   
-    productosquecultiva.get(producto.getNombre()).getFechas().get(fecha).setFecha(fecha);
-    productosquecultiva.get(producto.getNombre()).getFechas().get(fecha).setLugardeproceso(m);
-    
-    }
+  
    
 
   
